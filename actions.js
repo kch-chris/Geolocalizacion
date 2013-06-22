382a1d78b10d03d34341f23c19653175f000f1a8
Christian Murillo <christian.murillo@nuvemtecnologia.mx>
2013-06-21 21:19:20 -0500

    asd

---
+++ actions.js
@@ -0,0 +1,28 @@
+//Actions
+
+function initialize(lat,lon) {
+				//Posición del mapa
+				var latlng = new google.maps.LatLng(lat, lon);
+				var myOptions = {
+					zoom: 8,
+					center: latlng,
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+				};
+				var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+				//Marcador
+				var marker = new google.maps.Marker({
+					position: latlng, 
+					map: map, 
+					title:"Mi posición"
+				});
+			}
+			
+$(document).ready(function(e) {
+	document.addEventListener("deviceready",function(){
+	navigator.geolocation.getCurrentPosition(function(p){
+		 initialize(p.coords.latitude,p.coords.longitude);
+		}, function(err){
+		navigator.notification.alert("Error:"+err.code,null,"Geolocalizacion","Aceptar");
+		});
+   	},false);
+});
